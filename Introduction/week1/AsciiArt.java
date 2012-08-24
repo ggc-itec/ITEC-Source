8ff0c105c12d12de35bdb356370db627f5d9d7eb
Tacksoo Im <tacksoo.im@gmail.com>
2012-08-23 23:20:20 -0400

    ascii art

---
+++ Introduction/week1/AsciiArt.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class AsciiArt {
+
+	public static void main(String[] args) {
+
+		String myString = "Hello World";
+		System.out.println(myString);
+		System.out.println(" /\\  ");
+		System.out.println("/  \\ ");
+		System.out.println("\\  / ");
+		System.out.println(" \\/  "); 
+	}
+}  
