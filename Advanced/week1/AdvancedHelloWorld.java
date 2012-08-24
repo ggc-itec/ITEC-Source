ada79c89fc6ae0e550295dd70cddd405028f606a
Tacksoo Im <tacksoo.im@gmail.com>
2012-08-23 22:31:25 -0400

    string examples

---
+++ Advanced/week1/AdvancedHelloWorld.java
@@ -0,0 +1,14 @@
+package week1;
+
+import java.util.Date;
+
+
+public class AdvancedHelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+	}
+
+}
--- Intermediate/week1/HelloWorld.java
+++ Intermediate/week1/HelloWorld.java
@@ -1,4 +1,4 @@
-package Intermediate.week1;
+package week1;
 
 public class HelloWorld {
 
