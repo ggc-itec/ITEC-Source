2b59af209f4e018a276bcf3c93226199bc9a20fe
Tacksoo Im <tacksoo.im@gmail.com>
2012-08-23 23:11:25 -0400

    hello world update

---
+++ Introduction/week1/HelloWorld.java
@@ -0,0 +1,29 @@
+package week1;
+
+public class HelloWorld {
+  
+   /*
+    *    ---   ---
+    *        |
+    *     :     :
+    *      \___/
+    * 
+    *   This is a multi-line comment
+    * 
+    * 
+    */
+    
+   // 8/21/2012 
+   // ITEC 2120
+   // author: dr. im
+   // this is a single-line comment
+   public static void main(String[] args) {
+  
+      System.out.println("Hello! \n My name is Dr. Im");
+      System.out.println("\tI am your professor in this class");
+      System.out.println("Welcome to ITEC 2120");
+      System.out.println("Hope you enjoy learning Java and programming"); 
+      
+   }
+  
+}
