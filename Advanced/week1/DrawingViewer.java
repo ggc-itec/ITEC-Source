ac9c81d1607cbe026d16effcfab3e57e0ef662d6
Tacksoo Im <tacksoo.im@gmail.com>
2012-08-28 17:42:56 -0400

    new

--- Advanced/week1/DrawingViewer.java
+++ Advanced/week1/DrawingViewer.java
@@ -8,33 +8,45 @@ import javax.swing.JFrame;
 
 public class DrawingViewer {
 
-	public static void main(String[] args) {
-		final Drawing d = new Drawing();
+	private Drawing d = new Drawing();
+
+	
+	class MyKeyListener implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO complete DOWN and LEFT
+			if(e.getKeyCode() == KeyEvent.VK_UP)
+				d.moveY();
+			if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+				d.moveX();
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public DrawingViewer() {
 		JFrame myFrame = new JFrame("Drawing");
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		myFrame.setLayout(new BorderLayout());
 		myFrame.add(d,BorderLayout.CENTER);
-		myFrame.addKeyListener(new KeyListener() {
-
-			@Override
-			public void keyPressed(KeyEvent e) {
-				// TODO complete DOWN and LEFT
-				if(e.getKeyCode() == KeyEvent.VK_UP)
-					d.moveY();
-				if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-					d.moveX();
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-			}
-
-			@Override
-			public void keyTyped(KeyEvent e) {				
-			}
-		
-		});
+		myFrame.addKeyListener(new MyKeyListener());
 		myFrame.setSize(500,500);
-		myFrame.setVisible(true);		
+		myFrame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+	   new DrawingViewer();
 	}
 }
