cda8be77e4bfc8eb82788b07fa4bc878c0906766
Tacksoo Im <tacksoo.im@gmail.com>
2012-09-18 15:33:27 -0400

    little changes

--- Intermediate/week02/ListenerExample.java
+++ Intermediate/week02/ListenerExample.java
@@ -1,4 +1,4 @@
-package week2;
+package week02;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
--- Intermediate/week02/MyConsoleApp.java
+++ Intermediate/week02/MyConsoleApp.java
@@ -1,4 +1,4 @@
-package week2;
+package week02;
 
 import java.io.File;
 import java.util.Scanner;
