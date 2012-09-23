128dbb2518d714462962a0a2e5ba46bda3f01234
Tacksoo Im <tacksoo.im@gmail.com>
2012-09-23 15:53:53 -0400

    update collections example

--- Advanced/week05/CollectionsExamples.java
+++ Advanced/week05/CollectionsExamples.java
@@ -2,9 +2,12 @@ package week05;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.Stack;
 import java.util.TreeSet;
 
 public class CollectionsExamples {
@@ -36,8 +39,22 @@ public class CollectionsExamples {
 		states.put("CA", "California");
 		states.put("SC", "South Carolina");
 		states.put("HI", "Hawaii");
-		System.out.println(states.get("GA")); // keys are like call numbers for
-												// objects
+		System.out.println(states.get("GA")); // keys are like call numbers for objects
+
+		// a stack is a abstract data structure but Java provides an implementation for us
+		Stack<Character> stack = new Stack<Character>();
+		String str = "example";
+		for( int i=0; i<str.length(); i++) 
+		{
+			stack.push(str.charAt(i));  // pushing a character to the top of the stack
+		}
+		
+		String rev = "";
+		while( !stack.isEmpty() )
+		{
+			rev = rev + stack.pop();  // poping a character off the top of the stack
+		}
+		System.out.println(rev);
 	}
 
 }
